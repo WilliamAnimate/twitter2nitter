a43e041e5cfbbcc13f02897e66ed4b333f7e1258
William Animate <75321465+WilliamAnimate@users.noreply.github.com>
2023-12-17 13:22:36 -0500

    x
    never gonna update the extension because im lazy lmao

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.webNavigation.onBeforeNavigate.addListener(function(d) {
 	const url = new URL(d.url);
-	if (url.hostname == 'twitter.com') {
+	if (url.hostname == 'twitter.com' || url.hostname == 'x.com') {
 		const newUrl = 'https://nitter.net' + url.pathname + url.search + url.hash;
 		chrome.tabs.update(d.tabId, {url: newUrl});
 	}
