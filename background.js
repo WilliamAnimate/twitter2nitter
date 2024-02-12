b528a22c130b8383cb181811c80d2d7db27477bd
William Animate <75321465+WilliamAnimate@users.noreply.github.com>
2024-02-12 18:36:37 -0500

    temp fix: redirect to nitter.x86-64-unknown-linux-gnu.zip
    main nitter is down. RIP.

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.webNavigation.onBeforeNavigate.addListener(function(d) {
 	const url = new URL(d.url);
 	if (url.hostname == 'twitter.com' || url.hostname == 'x.com') {
-		const newUrl = 'https://nitter.net' + url.pathname + url.search + url.hash;
+		const newUrl = 'https://nitter.x86-64-unknown-linux-gnu.zip' + url.pathname + url.search + url.hash;
 		chrome.tabs.update(d.tabId, {url: newUrl});
 	}
 });
