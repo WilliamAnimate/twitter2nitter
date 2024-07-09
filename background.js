80746c1571a86a3b819146fd97b615efdfe5f335
William <75321465+WilliamAnimate@users.noreply.github.com>
2024-07-08 20:34:14 -0400

    add more boilerplate
    professionality++

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 chrome.webNavigation.onBeforeNavigate.addListener(function(d) {
 	const url = new URL(d.url);
 	if (url.hostname == 'twitter.com' || url.hostname == 'x.com') {
